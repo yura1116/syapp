e9e38b6c02d85dda28934dfeec447ea7e7cd817e
xh <1637540706@qq.com>
2023-09-15 15:38:34 +0800

    前台页面UI

---
+++ app/src/main/java/com/example/syapp/StartActivity.java
@@ -0,0 +1,25 @@
+package com.example.syapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Button qdbtn = (Button)findViewById(R.id.start_now);
+        qdbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

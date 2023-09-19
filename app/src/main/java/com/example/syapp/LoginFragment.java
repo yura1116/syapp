d0e7e07d96bd1d40e85b88ffc16a74b37ff05faf
xh <1637540706@qq.com>
2023-09-19 10:49:46 +0800

    注册页面

--- app/src/main/java/com/example/syapp/LoginFragment.java
+++ app/src/main/java/com/example/syapp/LoginFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 
 public class LoginFragment extends Fragment {
+    private Button login_btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         View view=inflater.inflate(R.layout.fragment_login, null);
         return view;
+    }
 
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        login_btn = getActivity().findViewById(R.id.button_login);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("点击登录按钮");
+            }
+        });
     }
 }

b8e1e9724edca6b85ed7e8451f6f8965e25e41c4
xh <1637540706@qq.com>
2023-09-19 10:21:53 +0800

    注册页面

--- app/src/main/java/com/example/syapp/RegFragment.java
+++ app/src/main/java/com/example/syapp/RegFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 
 public class RegFragment extends Fragment {
+    private Button reg_btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -16,4 +18,17 @@ public class RegFragment extends Fragment {
         return view;
 
     }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        reg_btn = getActivity().findViewById(R.id.button_reg);
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("点击注册按钮");
+            }
+        });
+    }
+
+
 }

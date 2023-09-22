cb1dc6c85e324083288aa82557c58e005b51520d
xh <1637540706@qq.com>
2023-09-22 13:59:36 +0800

    注册页面

--- app/src/main/java/com/example/syapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/syapp/ui/home/HomeFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.example.syapp.R;
 import com.example.syapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private Button home_syyy_btn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +29,33 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        home_syyy_btn = getActivity().findViewById(R.id.home_syyy);
+        home_syyy_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("点击适用语言按钮");
+
+                // 初始化Python解释器
+                if (!Python.isStarted()) {
+                    Python.start(new AndroidPlatform(getActivity()));
+                }
+                // 导入Python模块
+                Python python = Python.getInstance();
+                PyObject pyModule = python.getModule("test_around_db");
+
+                // 调用Python函数
+                PyObject result = pyModule.callAttr("test_around_db");
+
+                // 处理Python函数的返回值
+                String pythonResult = result.toJava(String.class);
+                System.out.println(pythonResult);
+            }
+        });
     }
 
     @Override
